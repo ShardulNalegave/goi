0c7b897020a99e44c40e1aaf435f401607b1b4f3
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-06-29 21:35:39 +0530

    Working on Colors

--- colors/color.go
+++ colors/color.go
@@ -1,12 +1,34 @@
 package colors
 
 // Color interface for color struct
-type Color interface{}
+type Color interface {
+	GetR() float32
+	GetG() float32
+	GetB() float32
+}
 
 // color struct
-type color struct{}
+type color struct {
+	R, G, B float32
+}
+
+func (c *color) GetR() float32 {
+	return c.R
+}
+
+func (c *color) GetG() float32 {
+	return c.G
+}
+
+func (c *color) GetB() float32 {
+	return c.B
+}
 
 // NewColor --> Creates a new color struct
-func NewColor() Color {
-	return &color{}
+func NewColor(r float32, g float32, b float32) Color {
+	return &color{
+		R: r,
+		G: g,
+		B: b,
+	}
 }
