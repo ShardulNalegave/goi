4011df9cf769e51f78d766e488b2c72ebaa74417
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-06-29 21:53:22 +0530

    Working on colors

--- canvas/canvas.go
+++ canvas/canvas.go
@@ -1,6 +1,7 @@
 package canvas
 
 import (
+	"goi/colors"
 	"log"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
@@ -11,6 +12,7 @@ type Canvas interface {
 	Init()
 	GetGlProgram() uint32
 	ClearScreen()
+	BackgroundColor(colors.Color)
 }
 
 // canvas struct
@@ -32,6 +34,11 @@ func (c *canvas) GetGlProgram() uint32 {
 
 func (c *canvas) ClearScreen() {
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+}
+
+func (c *canvas) BackgroundColor(col colors.Color) {
+	colorArray := col.Normalize()
+	gl.ClearColor(colorArray[0], colorArray[1], colorArray[2], colorArray[3])
 }
 
 // NewCanvas --> Creates a new canvas struct
--- colors/color.go
+++ colors/color.go
@@ -5,12 +5,13 @@ type Color interface {
 	GetR() float32
 	GetG() float32
 	GetB() float32
-	Normalize() [3]float32
+	GetA() float32
+	Normalize() [4]float32
 }
 
 // color struct
 type color struct {
-	R, G, B float32
+	R, G, B, A float32
 }
 
 func (c *color) GetR() float32 {
@@ -25,19 +26,25 @@ func (c *color) GetB() float32 {
 	return c.B
 }
 
-func (c *color) Normalize() [3]float32 {
-	return [3]float32{
+func (c *color) GetA() float32 {
+	return c.A
+}
+
+func (c *color) Normalize() [4]float32 {
+	return [4]float32{
 		c.R / 255,
 		c.G / 255,
 		c.B / 255,
+		c.A / 255,
 	}
 }
 
 // NewColor --> Creates a new color struct
-func NewColor(r float32, g float32, b float32) Color {
+func NewColor(r float32, g float32, b float32, a float32) Color {
 	return &color{
 		R: r,
 		G: g,
 		B: b,
+		A: a,
 	}
 }
