11a5e11e8601e0028b9a9aaed495a48da401c47e
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-06-29 22:38:23 +0530

    Working on buffers

--- buffers/vao.go
+++ buffers/vao.go
@@ -1,12 +1,36 @@
 package buffers
 
+import "github.com/go-gl/gl/v4.1-core/gl"
+
 // VAO inteface for vao struct
-type VAO interface{}
+type VAO interface {
+	init()
+	GetID() uint32
+	Use(func())
+}
 
 // vao struct
-type vao struct{}
+type vao struct {
+	id uint32
+}
+
+func (v *vao) init() {
+	gl.GenVertexArrays(0, &v.id)
+}
+
+func (v *vao) GetID() uint32 {
+	return v.id
+}
+
+func (v *vao) Use(handler func()) {
+	gl.BindVertexArray(v.id)
+	handler()
+	gl.BindVertexArray(0)
+}
 
 // NewVAO --> Creates a new vao struct
 func NewVAO() VAO {
-	return &vao{}
+	v := vao{}
+	v.init()
+	return &v
 }
--- buffers/vbo.go
+++ buffers/vbo.go
@@ -1,12 +1,20 @@
 package buffers
 
 // VBO inteface for vao struct
-type VBO interface{}
+type VBO interface {
+	GetID() uint32
+}
 
 // vao struct
-type vbo struct{}
+type vbo struct {
+	id uint32
+}
+
+func (v *vbo) GetID() uint32 {
+	return v.id
+}
 
 // NewVBO --> Creates a new vbo struct
-func NewVBO() VAO {
+func NewVBO() VBO {
 	return &vbo{}
 }
