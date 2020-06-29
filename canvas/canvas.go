3a787d8fa0241af95cbddcfa490eee2a0facc925
Shardul Nalegave <nalegaveshardul40@gmail.com>
2020-06-29 16:47:31 +0530

    Working on Canvas

--- canvas/canvas.go
+++ canvas/canvas.go
@@ -1,12 +1,36 @@
 package canvas
 
+import (
+	"log"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+)
+
 // Canvas interface for canvas struct
-type Canvas interface{}
+type Canvas interface {
+	init()
+	GetGlProgram() uint32
+}
 
 // canvas struct
-type canvas struct{}
+type canvas struct {
+	glProgram uint32
+}
+
+func (c *canvas) init() {
+	if err := gl.Init(); err != nil {
+		log.Panic(err)
+	}
+	c.glProgram = gl.CreateProgram()
+}
+
+func (c *canvas) GetGlProgram() uint32 {
+	return c.glProgram
+}
 
 // NewCanvas --> Creates a new canvas struct
 func NewCanvas() Canvas {
-	return &canvas{}
+	canv := canvas{}
+	canv.init()
+	return &canv
 }
